500afd0d2ee1458bd8e36d930ceb890cf8bf75bd
sarahtrefethen <49411879+sarahtrefethen@users.noreply.github.com>
2026-04-20 14:36:53 -0400

    build

--- HUBSPOT_TO_S3/python/hubspot2s3/__main__.py
+++ HUBSPOT_TO_S3/python/hubspot2s3/__main__.py
@@ -18,7 +18,7 @@ def main():
     secrets = Secrets.from_env()
     context = Context.from_env()
 
-    logger.error("hi there!!")
+    logger.error("hi there!!!")
 
     print(f'This run is in mode: {context.run_mode}', file=sys.stderr)
     print(config, file=sys.stderr)
